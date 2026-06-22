fcbf91f91b2b1e279ae19de3fb808e55a351d18e
Sehaj Kaur <206737757+Sehaj-Kaur-git@users.noreply.github.com>
2026-06-22 19:23:02 +0530

    fixed auth controller

--- Backend/src/controllers/auth.controller.js
+++ Backend/src/controllers/auth.controller.js
@@ -35,7 +35,12 @@ async function registerUserController (req, res) {
         { expiresIn: '1d' }
     )
 
-    res.cookie('token', token)
+    res.cookie("token", token, {
+    httpOnly: true,
+    secure: true,
+    sameSite: "none",
+    maxAge: 24 * 60 * 60 * 1000,
+    });
 
     res.status(201).json({
         message: 'User registered successfully',
@@ -70,7 +75,14 @@ async function loginUserController (req, res) {
         process.env.JWT_SECRET,
         { expiresIn: '1d' }
     )
-    res.cookie('token', token)
+    
+    res.cookie("token", token, {
+    httpOnly: true,
+    secure: true,
+    sameSite: "none",
+    maxAge: 24 * 60 * 60 * 1000,
+    });
+
     res.status(200).json({
         message: 'User logged in successfully',
         user: {
@@ -88,7 +100,11 @@ async function logoutUserController (req, res) {
     if (token) {
         await TokenBlacklistModel.create({ token })
     }
-    res.clearCookie('token')
+    res.clearCookie("token", {
+    httpOnly: true,
+    secure: true,
+    sameSite: "none",
+    });
     res.status(200).json({ message: 'User logged out successfully' })
 }
 
